0e90ab50ba796eb6bc80f27979f1e411b4209d89
anuragupperwal <48147588+anuragupperwal@users.noreply.github.com>
2019-10-12 14:39:49 +0530

    Data type and some corrections

--- BegineerAdult_251902060.c
+++ BegineerAdult_251902060.c
@@ -1,15 +1,16 @@
 /*Begineer or Adult checker*/
 #include <stdio.h>
-main ()
+int main ()
 {
 	int a;
 	printf("Enter your age");
 	scanf("%d",&a);
 	if (a>=18)
 	{
-	 printf("You are an adult");
+	 printf("You are an Adult :P");
 	 }
 	else{
-	 printf("You are a begineer");
+	 printf("You are a Begineer :)");
 	 } 
+	return 0;
 }
